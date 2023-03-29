d9080646edc4444b2e89c0506f7155e0ace94fe4
Konrad Łapka <konrad.lapka58@hotmail.com>
2023-03-29 19:09:51 +0200

    Add spring boot

---
+++ backend/Zjadbyco/src/test/java/com/zjadbyco/ZjadbycoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjadbyco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZjadbycoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
