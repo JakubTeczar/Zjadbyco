b98afe06195f37d50446035f0a81852a0e603a34
JakubTeczar <jakubteczar6@gmial.com>
2023-03-29 21:22:02 +0200

    2 dzialajace serwery tf tf

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,8 +1,9 @@
+import React from "react";
 
 function RootLayout(){
     return(
         <>
-            <h1>rooot</h1>        
+            <h1>tu menu</h1>        
             <h1>rooot</h1>
         </>
     );
