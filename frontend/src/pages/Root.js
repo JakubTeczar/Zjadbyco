6d1160523261fd602feeba0fda85556cc8381dcf
JakubTeczar <jakubteczar6@gmial.com>
2023-03-29 15:43:28 +0200

    Wstepna segregacja plikow i folderow

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,40 +1,11 @@
-import { useEffect } from 'react';
-import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
-// import MainNavigation from '../components/MainNavigation';
-import { getTokenDuration } from '../util/auth';
-
-function RootLayout() {
-  // const token = useLoaderData();
-  // const submit = useSubmit();
-  // // const navigation = useNavigation();
-  // useEffect(() => {
-  //   if (!token) {
-  //     return;
-  //   }
-
-  //   if (token === 'EXPIRED') {
-  //     submit(null, { action: '/logout', method: 'post' });
-  //     return;
-  //   }
-
-  //   const tokenDuration = getTokenDuration();
-  //   console.log(tokenDuration);
-
-  //   setTimeout(() => {
-  //     submit(null, { action: '/logout', method: 'post' });
-  //   }, tokenDuration);
-  // }, [token, submit]);
-
-  return (
-    <>
-      <h1>dziala ?</h1>
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
-}
+function RootLayout(){
+    return(
+        <>
+            <h1>rooot</h1>        
+            <h1>rooot</h1>
+        </>
+    );
+};
 
 export default RootLayout;
