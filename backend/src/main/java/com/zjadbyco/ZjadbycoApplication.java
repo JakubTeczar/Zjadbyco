ac08534619c6b23833bf1fbe929cca61614121fb
Konrad Łapka <konrad.lapka58@hotmail.com>
2023-04-01 01:54:50 +0200

    baza dziala na serwerze

--- backend/src/main/java/com/zjadbyco/ZjadbycoApplication.java
+++ backend/src/main/java/com/zjadbyco/ZjadbycoApplication.java
@@ -3,7 +3,7 @@ package com.zjadbyco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication()
 public class ZjadbycoApplication {
 
     public static void main(String[] args) {
