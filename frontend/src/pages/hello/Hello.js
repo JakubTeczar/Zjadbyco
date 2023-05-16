4478ed905f3972a42f62fc68ff8b6a7e8f77e686
JakubTeczar <jakubteczar6@gmial.com>
2023-05-16 13:24:14 +0200

    poprawa responsywnego layoutu

--- frontend/src/pages/hello/Hello.js
+++ frontend/src/pages/hello/Hello.js
@@ -6,8 +6,8 @@ function Hello () {
         <div className="hello"> 
             <h1>Witaj</h1>
             <div className="hello__box">
-            <h4 className="hello__box--text1"><b>Ta aplikacja pomoże ci</b> w planowaniu posiłków podczas dnia <br></br> oraz tworzenia listy zakupów</h4>
-            <h4 className="hello__box--text2">Przed pierwszym uruchomieniem naszej aplikacji <br></br> <b>podaj nam co lubisz jeść.</b></h4>
+            <h4 className="hello__box--text1"><b>Ta aplikacja pomoże ci</b> w planowaniu posiłków podczas dnia <span></span> oraz tworzenia listy zakupów</h4>
+            <h4 className="hello__box--text2">Przed pierwszym uruchomieniem naszej aplikacji <span></span> <b>podaj nam co lubisz jeść.</b></h4>
             </div>
             <Link to="chooseElements/grainProducts"><button className="hello__btn">Zaczynajmy</button></Link>
         </div>
