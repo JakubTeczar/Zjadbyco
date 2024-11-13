21d62538a17cc32b030139a38178e5fa2855202c
JakubTeczar <jakubteczar6@gmial.com>
2024-11-14 00:46:04 +0100

    fix App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouterProvider, createHashRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import "../src/css/pages/styles.css";
 // import ErrorPage from './pages/Error';
 import RootLayout from './pages/Root';
@@ -23,7 +23,7 @@ import AuthPage from './pages/authentication/AuthPage';
 import AddNewElements , {action as AddOwnElement} from './pages/settings/AddNewElements';
 
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: 'authentication/:state',
     element: <AuthPage/>,
